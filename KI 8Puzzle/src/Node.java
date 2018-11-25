69df96a9fa2bd10efee9c541da299a041bcae36d
Ekaterina Tushkova <tushkova.e@gmail.com>
2018-11-25 15:47:36 +0100

    implemented DFS, added a function to create text files with solution logs, fixed a bug where BFS would attempt to solve an already solved puzzle by moving back and forth once, fixed solution output order

--- KI 8Puzzle/src/Node.java
+++ KI 8Puzzle/src/Node.java
@@ -1,24 +1,24 @@
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
 
-    public List<Node> children = new ArrayList<Node>();
+    public List<Node> children = new ArrayList<>();
     public Node parent;
-    public int[] puzzle = new int[9];
-    public int x = 0;
+    public int[] puzzle;
+    private int x = 0;
     private int col = 3;
-    public Node(int[] p){
+
+    public Node(int[] p) {
         puzzle = p;
     }
 
-    public void MoveRight(int[]p,int i){
-        if(i % col < col -1){
+    public void moveRight(int[] p, int i) {
+        if (i % col < col - 1) {
             int[] pc = new int[9];
-            copyPuzzle(pc,p);
-            int temp = pc[i+1];
-            pc[i+1] = pc[i];
+            copyPuzzle(pc, p);
+            int temp = pc[i + 1];
+            pc[i + 1] = pc[i];
             pc[i] = temp;
             Node child = new Node(pc);
             children.add(child);
@@ -26,12 +26,12 @@ public class Node {
         }
     }
 
-    public void MoveLeft(int[]p,int i){
-        if(i % col > 0){
+    public void moveLeft(int[] p, int i) {
+        if (i % col > 0) {
             int[] pc = new int[9];
-            copyPuzzle(pc,p);
-            int temp = pc[i-1];
-            pc[i-1] = pc[i];
+            copyPuzzle(pc, p);
+            int temp = pc[i - 1];
+            pc[i - 1] = pc[i];
             pc[i] = temp;
             Node child = new Node(pc);
             children.add(child);
@@ -39,12 +39,12 @@ public class Node {
         }
     }
 
-    public void MoveUp(int[]p,int i){
-        if(i - col >= 0){
+    public void moveUp(int[] p, int i) {
+        if (i - col >= 0) {
             int[] pc = new int[9];
-            copyPuzzle(pc,p);
-            int temp = pc[i-3];
-            pc[i-3] = pc[i];
+            copyPuzzle(pc, p);
+            int temp = pc[i - 3];
+            pc[i - 3] = pc[i];
             pc[i] = temp;
             Node child = new Node(pc);
             children.add(child);
@@ -52,12 +52,12 @@ public class Node {
         }
     }
 
-    public void MoveDown(int[]p,int i){
-        if(i + col  < puzzle.length){
+    public void moveDown(int[] p, int i) {
+        if (i + col < puzzle.length) {
             int[] pc = new int[9];
-            copyPuzzle(pc,p);
-            int temp = pc[i+3];
-            pc[i+3] = pc[i];
+            copyPuzzle(pc, p);
+            int temp = pc[i + 3];
+            pc[i + 3] = pc[i];
             pc[i] = temp;
             Node child = new Node(pc);
             children.add(child);
@@ -65,61 +65,58 @@ public class Node {
         }
     }
 
-    public void expandNode(){
-        for(int i = 0; i < puzzle.length; i++){
-            if(puzzle[i] == 0){
-                x=i;
+    public void expandNode() {
+        for (int i = 0; i < puzzle.length; i++) {
+            if (puzzle[i] == 0) {
+                x = i;
+                break;
             }
         }
-        MoveDown(puzzle,x);
-        MoveLeft(puzzle,x);
-        MoveRight(puzzle,x);
-        MoveUp(puzzle,x);
-    }
-    public void setPuzzle(int[] p){
-        for (int i =0;i<puzzle.length;i++){
-            this.puzzle[i] = p[i];
-        }
+        moveDown(puzzle, x);
+        moveLeft(puzzle, x);
+        moveRight(puzzle, x);
+        moveUp(puzzle, x);
     }
 
-    public void copyPuzzle(int[]a,int[] b){
-        for(int i=0;i<a.length;i++){
+    public void copyPuzzle(int[] a, int[] b) {
+        for (int i = 0; i < a.length; i++) {
             a[i] = b[i];
         }
     }
-    public boolean GoalTest() {
+
+    public boolean goalTest() {
         boolean isGoal = true;
         int[] goal = {
-                1,2,3,
-                8,0,4,
-                7,6,5
+                1, 2, 3,
+                8, 0, 4,
+                7, 6, 5
         };
-        for(int i = 0;i<goal.length;i++){
-            if(puzzle[i] != goal[i]){
-                isGoal = false;
-            }
-        }
+        if (!this.isSamePuzzle(goal)) isGoal = false;
         return isGoal;
     }
 
-    public boolean isSamePuzzle(int[] p){
+    public boolean isSamePuzzle(int[] p) {
         boolean samePuzzle = true;
-        for(int i = 0; i < p.length; i++){
-            if(puzzle[i] != p[i]){
+        for (int i = 0; i < p.length; i++) {
+            if (puzzle[i] != p[i]) {
                 samePuzzle = false;
+                break;
             }
         }
         return samePuzzle;
     }
-    public void prinzPuzzle(){
-        System.out.println();
+
+    @Override
+    public String toString() {
+        StringBuilder line = new StringBuilder("\n");
         int m = 0;
-        for(int i=0;i<col;i++){
-            for(int j=0;j<col;j++){
-                System.out.print(" "+this.puzzle[m]+" ");
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < col; j++) {
+                line.append(" ").append(this.puzzle[m]).append(" ");
                 m++;
             }
-            System.out.println("");
+            line.append("\n");
         }
+        return line.toString();
     }
 }
--- KI 8Puzzle/src/Solver.java
+++ KI 8Puzzle/src/Solver.java
@@ -1,97 +1,157 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Solver {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         int[] puzzle = {
-                2,8,3,
-                1,6,4,
-                7,0,5
+                2, 8, 3,
+                1, 6, 4,
+                7, 0, 5
         };
 
         Node root = new Node(puzzle);
-        final long timeStart = System.nanoTime();
-        List<Node> solution = Breitensuche(root);
-        final long timeEnd = System.nanoTime();
-        System.out.println("Zeit: "+ (timeEnd-timeStart)+ " Nanosekunden");
-        System.out.println("Pfadkosten: "+solution.size());
 
-        if(solution.size()>0){
-            for(int i=solution.size()-1;i>=0;i--){
-                solution.get(i).prinzPuzzle();
-            }
-        }else{
-            System.out.println("No path found");
-        }
+        List<Node> solutionBFS = suche(root, "breitensuche");
+        List<Node> solutionDFS = suche(root, "tiefensuche");
+
+        System.out.println("-- BREITENSUCHE --");
+        printSolution(solutionBFS);
+        System.out.println();
+        logSolution(solutionBFS, "Breitensuche");
+
+
+        System.out.println("-- TIEFENSUCHE --");
+        printSolution(solutionDFS);
+        System.out.println();
+        logSolution(solutionDFS, "Tiefensuche");
     }
 
+
     /**
-     * Simple Breitensuche mit hilfe einer einfachen queue
-     * @param root
-     * @return
+     * Simple Breitensuche und Tiefensuche mit hilfe einer einfachen queue
+     *
+     * @param root      - Anfangsknoten
+     * @param algorithm - Name des Algorithmus als String
+     * @return List<Node> - Lösungsweg
      */
-    public static List<Node> Breitensuche(Node root){
+    public static List<Node> suche(Node root, String algorithm) {
         List<Node> pathToSolution = new ArrayList<>();
         List<Node> openList = new ArrayList<>();
         List<Node> closedList = new ArrayList<>();
 
         openList.add(root);
-        boolean reachedGoal = false;
 
-        //Solange wir das goal nicht erreicht haben und noch nodes da sind suchen wir
-        while(!openList.isEmpty() && !reachedGoal){
+        //Solange noch knoten da sind suchen wir
+        while (!openList.isEmpty()) {
+
             //abarbeiten des obersten knotens
             Node currentNode = openList.get(0);
             closedList.add(currentNode);
             openList.remove(0);
 
+            // ist dieser knoten das goal?
+            if (currentNode.goalTest()) {
+                System.out.println("Goal found!");
+                pathTrace(pathToSolution, currentNode);
+                return pathToSolution;
+            }
+
             //alle richtungen gehen
             currentNode.expandNode();
 
-            //alle childs abfangen
-            for(int i =0;i < currentNode.children.size();i++){
-                Node currentChild = currentNode.children.get(i);
-                if(currentChild.GoalTest()){
-                    System.out.println("Goal found!");
-                    reachedGoal = true;
-                    pathTrace(pathToSolution,currentChild);
-                }
-                //wenn der Goaltest nicht durchgeht wird hier das currentChild an die openList angehängt
-                // Beispiel pathtracing : a
-                //                       b,c,d
-                //                      efg hij klm
-                // erste queue a, dann bcd, dann cdefg dann defghij und so weiter
-                if(!contains(openList,currentChild) && !contains(closedList,currentChild)){
-                    openList.add(currentChild);
-                }
+            // der einzige unterschied ist, wie man die queue (aka openList) bildet
+            switch (algorithm) {
+                case "breitensuche":
+                    //alle children abfangen
+                    for (int i = 0; i < currentNode.children.size(); i++) {
+                        Node currentChild = currentNode.children.get(i);
 
+                        // currentChild wird an die openList angehängt
+                        // Beispiel pathtracing : a
+                        //                       b,c,d
+                        //                      efg hij klm
+                        // erste queue a, dann bcd, dann cdefg dann defghij und so weiter
+
+                        if (!contains(openList, currentChild) && !contains(closedList, currentChild)) {
+                            openList.add(currentChild);
+                        }
+                    }
+                    break;
+                case "tiefensuche":
+                    // alle children immer am ANFANG der openList hinzufügen (Stack-Prinzip)
+                    for (int i = currentNode.children.size() - 1; i >= 0; i--) {
+                        Node currentChild = currentNode.children.get(i);
+
+                        // Beispiel pathtracing :    a
+                        //                       b   c    d
+                        //                     efg  hij  klm
+                        // queue: a -> bcd -> efgcd -> fgcd -> gcd -> cd -> hijd -> ijd ... usw
+
+                        if (!contains(openList, currentChild) && !contains(closedList, currentChild)) {
+                            openList.add(0, currentChild);
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid algorithm name");
+                    break;
             }
-
         }
-
         return pathToSolution;
     }
 
     //rückwärts die parents durchgehen
-    public static void pathTrace(List<Node> path, Node goal){
-        System.out.println("Tracing path");
+    public static void pathTrace(List<Node> path, Node goal) {
+        System.out.println("Tracing path\n");
         Node current = goal;
         path.add(current);
-        while(current.parent != null){
+        while (current.parent != null) {
             current = current.parent;
             path.add(current);
-
         }
     }
 
-    public static boolean contains(List<Node> a,Node b){
+    public static boolean contains(List<Node> a, Node b) {
         boolean contains = false;
-        for(int i = 0; i < a.size(); i++){
-            if(a.get(i).isSamePuzzle(b.puzzle)){
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i).isSamePuzzle(b.puzzle)) {
                 contains = true;
+                return contains;
             }
         }
         return contains;
+    }
+
+    public static void printSolution(List<Node> solution) {
+        if (solution.size() > 0) {
+            for (int i = solution.size() - 1; i >= 0; i--) {
+                System.out.print(solution.get(i).toString());
+            }
+            System.out.println("\n" + solution.size() + " Schritt(e)\n");
+        } else {
+            System.out.println("No path found");
+        }
+    }
+
+    public static void logSolution(List<Node> solution, String algorithm) {
+        try {
+            Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(algorithm + ".txt"), "utf-8"));
+            writer.write("Lösung mit " + algorithm + ":\n");
+            if (solution.size() > 0) {
+                for (int i = solution.size() - 1; i >= 0; i--) {
+                    writer.append(solution.get(i).toString());
+                }
+                writer.append("\n" + solution.size() + " Schritt(e)\n");
+            } else {
+                writer.append("No path found");
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
